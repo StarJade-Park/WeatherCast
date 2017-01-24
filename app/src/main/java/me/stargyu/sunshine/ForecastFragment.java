5191d93346c8c65c3984306a7d8e55394fe54d44
psk7142 <psk7142@naver.com>
2017-01-24 23:48:03 +0900

    Initial commit

---
+++ app/src/main/java/me/stargyu/sunshine/ForecastFragment.java
@@ -0,0 +1,272 @@
+package me.stargyu.sunshine;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    private final String LOG_TAG = ForecastFragment.class.getSimpleName();
+
+    ArrayAdapter<String> mForecastAdapter;
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forecast_fragment_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_refresh) {
+            FetchWeatherTask fetchWeatherTask = new FetchWeatherTask();
+            fetchWeatherTask.execute("Seoul,kr"); // excute만 부르면 내부에서 파이프라인 단계 거침
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        String[] data = {"mon - 123", "test", "android1",
+                "android2", "android3", "android4", "android5",
+                "android7", "androidex", "androidasdf"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+        // m 매개변수, 자바 클래스 내에 있는 속성
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast, // R - res
+                R.id.list_item_forecast,
+                weekForecast
+        );
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.list_view_forecast); //
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+
+    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+            throws JSONException {
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject temperatureInfo = dayInfo.getJSONObject("temp");
+
+        return temperatureInfo.getDouble("max");
+    }
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> { // 들어오고, process, 나가는 값
+        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName(); // 습관 들일 것, static을 포함해야하는 경우 있음
+
+        @NonNull
+        private String getReadableDateString(long time) {
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        private String formatHighLows(double high, double low) {
+            long roundedHigh = Math.round(high);
+            long roundedLow = Math.round(low);
+
+            String highLowStr = roundedHigh + "/" + roundedLow;
+            return highLowStr;
+        }
+
+        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
+                throws JSONException {
+
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            dayTime = new Time();
+
+            String[] resultStrs = new String[numDays];
+            for(int i = 0; i < weatherArray.length(); i++) {
+                String day;
+                String description;
+                String highAndLow;
+
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                long dateTime;
+
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                day = getReadableDateString(dateTime);
+
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                description = weatherObject.getString(OWM_DESCRIPTION);
+
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                double high = temperatureObject.getDouble(OWM_MAX);
+                double low = temperatureObject.getDouble(OWM_MIN);
+
+                highAndLow = formatHighLows(high, low);
+                resultStrs[i] = day + " - " + description + " - " + highAndLow;
+            }
+
+            for (String s : resultStrs) {
+//                Log.v(LOG_TAG, "Forecast entry: " + s);
+            }
+            return resultStrs;
+        }
+
+        @Override
+        protected String[] doInBackground(String... params) { // ...: 가변인자
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String forecastJsonStr = null;
+
+            String format = "JSON";
+            String units = "metrics";
+            int numDays = 7;
+
+            try {
+                final String FORECAST_BASE_URL = "http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String QUERY_PARAM = "q";
+                final String FORMAT_PARAM = "mode";
+                final String UNITS_PARAM = "units";
+                final String DAYS_PARAM = "cnt";
+                final String APPID_PARAM = "APPID";
+
+                Uri uri = Uri.parse(FORECAST_BASE_URL).buildUpon().
+                        appendQueryParameter(QUERY_PARAM, params[0]).
+                        appendQueryParameter(FORMAT_PARAM, format).
+                        appendQueryParameter(UNITS_PARAM, units).
+                        appendQueryParameter(DAYS_PARAM, Integer.toString(numDays)).
+                        appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_WEATHER_MAP_API_KEY).
+                        build();
+                URL url = new URL(uri.toString());
+//                Log.v(LOG_TAG, "URL: " + url.toString());
+
+//                URL url = new URL(
+//                        "http://api.openweathermap.org/data/2.5/forecast/daily" +
+//                                "?q=Seoul,kr&mode=json&units=metrics&cnt=7&" +
+//                                "appid=240b6a61733c22512ac314e63082e69b"
+//                );
+            /*
+            * android.os.NetworkOnMainThreadException
+            * Network 관련된 operation 을 Main Thread 에서 수행했을 때 발생하는 Exception
+            * */
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    forecastJsonStr = null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    forecastJsonStr = null;
+                }
+                forecastJsonStr = buffer.toString();
+
+//                Log.v(LOG_TAG, "Forecast JSON String: " + forecastJsonStr);
+            } catch (IOException e) {
+                Log.e("ForecastFragment", "Error ", e);
+                forecastJsonStr = null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("ForecastFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                return getWeatherDataFromJson(forecastJsonStr, numDays);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            if(strings != null) {
+                mForecastAdapter.clear();
+//                for(String dayForecastStr : strings) {
+//                    mForecastAdapter.add(dayForecastStr);
+//                }
+                mForecastAdapter.addAll(strings);
+            }
+        }
+    }
+
+}
