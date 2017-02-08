182e9184ac3146af89dbd8d7216ec6741f41bd78
psk7142 <psk7142@naver.com>
2017-02-08 20:09:19 +0900

    `17.02.08. 7장 수업(1) 실습 2

--- app/src/main/java/me/stargyu/sunshine/ForecastAdapter.java
+++ app/src/main/java/me/stargyu/sunshine/ForecastAdapter.java
@@ -11,6 +11,20 @@ import android.widget.TextView;
 
 public class ForecastAdapter extends CursorAdapter {
 
+    private static final int VIEW_TYPE_TODAY = 0; // 시작은 무조건 0
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
     }
@@ -34,9 +48,21 @@ public class ForecastAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
+//        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
 
-        return view;
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+
+        switch (viewType) {
+            case VIEW_TYPE_TODAY:
+                layoutId = R.layout.list_item_forecast_today;
+                break;
+            case VIEW_TYPE_FUTURE_DAY:
+                layoutId = R.layout.list_item_forecast;
+                break;
+        }
+
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     @Override
